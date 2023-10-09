7698d7b5583360f9ee15c09d8ef26c76e600ca3f
glaina <glaina@miners.utep.edu>
2023-10-09 00:48:44 -0600

    code refined, error handling added

--- mytar.py
+++ mytar.py
@@ -1,108 +1,87 @@
 import os
+import sys
 
-# return file descriptor
+# Opens the specified file and returns its file descriptor.
 def open_file(filename, mode):
     fd = os.open(filename, mode)
     return fd
 
-# returns file size
+# Returns the size of the file with the given file descriptor.
 def get_file_size(fd):
     size = os.lseek(fd, 0, os.SEEK_END)
+    os.lseek(fd, 0, os.SEEK_SET)  # Reset the file pointer to the beginning after getting size.
     return size
 
-# reads file content in blocks of 2500 bytes
+# Generator function to read content from the file in blocks.
 def read_block(fd, block_size = 2500):
-    while 1:
+    while True:
         data = os.read(fd, block_size)
-
         if data:
             yield data
         else:
             break
-    
-# write data to destination fd
+
+# Writes data to the specified file descriptor.
 def write_content(fd, data):
     bytes_written = 0
-
     while (bytes_written < len(data)):
         written = os.write(fd, data[bytes_written:])
         bytes_written += written
     return bytes_written
-    
-# encodes and frames data for archiving
-def framer(filename, content):
-    # check if filename is in bytes, if not convert it
-    name_bytes = filename if isinstance(filename, bytes) else filename.encode()
-    name_length = len(filename)
-    name_length_bytes = name_length.to_bytes(2, 'big') # constrain filename to 2 bytes
-    content_length = len(content)
-    content_length_bytes = content_length.to_bytes(8, 'big') # constrain content length to 8 bytes
 
+# Encodes and frames the filename and content for archiving.
+def framer(filename, content):
+    name_bytes = filename.encode()  # Convert filename to bytes.
+    name_length = len(name_bytes)
+    name_length_bytes = name_length.to_bytes(2, 'big')  # Filename length: 2 bytes
+    content_length_bytes = len(content).to_bytes(8, 'big')  # Content length: 8 bytes
+
+    # Return the combined byte stream: [name_length | name | content_length | content]
     return name_length_bytes + name_bytes + content_length_bytes + content
-    
-# decodes framed data
+
+# Decodes the framed data to retrieve the original filename and content.
 def deframer(buffer):
     filename_length = int.from_bytes(buffer[:2], 'big')
     filename = buffer[2:2+filename_length].decode()
     content_length = int.from_bytes(buffer[2+filename_length:10+filename_length], 'big')
     content = buffer[10+filename_length:10+filename_length+content_length]
     return filename, content
-     
-# archives files
+
+# Archives the specified files and writes to stdout.
 def create(files):
-    # iterate through files
     for fyle in files:
-        # get file descriptor
         fd = open_file(fyle, os.O_RDONLY)
-        content = b""
-        
-        #read file in blocks until EOF
-        for block in read_block(fd):
-            content += block
-
+        content = b"".join(read_block(fd))  # Combine all blocks to form the content.
         framed_data = framer(fyle, content)
-
-        # write framed data to stdout
-        write_content(1, framed_data)
-
-        # close fd
+        write_content(1, framed_data)  # Write to stdout (file descriptor 1).
         os.close(fd)
-    
-# extracts archived files
+
+# Extracts and restores files from the archived data read from stdin.
 def extract():
     buffer = b""
-    
     while True:
-        data = os.read(0, 1024)
+        data = os.read(0, 1024)  # Read from stdin (file descriptor 0).
         if not data:
             break
-        
         buffer += data
 
+        # Continue until all data in the buffer is processed.
         while buffer:
             filename, content = deframer(buffer)
             fd = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
             os.write(fd, content)
             os.close(fd)
-            
             frame_length = 10 + len(filename.encode()) + len(content)
             buffer = buffer[frame_length:]
 
-    
-    
-# reports error message to stderr
+# Reports errors by writing the message to stderr.
 def report_error(message):
     os.write(2, (message + '\n').encode())
-    
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     mode = sys.argv[1]
-    
-    # Create mode
-    if mode == "c":
+    if mode == "c":  # Create mode
         files = sys.argv[2:]
         create(files)
-    
-    # Extract mode
-    elif mode == "x":
+    elif mode == "x":  # Extract mode
         extract()
